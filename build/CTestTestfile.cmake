30492675d1e98a8ff0054ec5f408e161e6a07bfc
SuperChange001 <chaoqian1119@gmail.com>
2016-12-02 18:34:15 +0000

    after make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/odroid/catkin_ws/src
+# Build directory: /home/odroid/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ros_astra_launch)
+subdirs(first_serial)
+subdirs(serial_bridge)
+subdirs(ros_astra_camera)
---
+++ build/first_serial/CMakeFiles/first_serial_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_serial_node.dir/first_serial.cpp.o"
+  "/home/odroid/catkin_ws/devel/lib/first_serial/first_serial_node.pdb"
+  "/home/odroid/catkin_ws/devel/lib/first_serial/first_serial_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_serial_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
