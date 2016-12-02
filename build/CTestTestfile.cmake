deb11d4d212f010698971099f7711ed89621c4d0
git <git@github.com>
2016-12-02 17:35:38 +0000

    second commit

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/catkin_ws/src
-# Build directory: /home/odroid/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(first_serial)
-subdirs(serial_bridge)
--- build/serial_bridge/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/catkin_ws/src/serial_bridge
-# Build directory: /home/odroid/catkin_ws/build/serial_bridge
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
