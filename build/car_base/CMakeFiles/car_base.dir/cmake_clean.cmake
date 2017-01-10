6dbd2a01b527649e38733613bea487416c0d9738
SuperChange001 <chaoqian1119@gmail.com>
2017-01-10 10:03:32 +0800

    I can Build map successfully.

---
+++ build/car_base/CMakeFiles/car_base.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_base.dir/src/car_base.cpp.o"
+  "/home/odroid/catkin_ws/devel/lib/car_base/car_base.pdb"
+  "/home/odroid/catkin_ws/devel/lib/car_base/car_base"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
